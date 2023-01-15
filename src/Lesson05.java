88a1847b69bf0373ae9439fe9b7761099387a89b
ChenShahaf <cshahaf@gmail.com>
2023-01-15 23:06:38 +0200

    lesson 5 update -Chen shahaf

--- src/Lesson05.java
+++ src/Lesson05.java
@@ -33,4 +33,65 @@ public class Lesson05 {
 
         return birth_year;
     }
+    /*
+	 * QUIZ 3 - 
+	 * 
+	 * You are given a function ‘combineStrings’ that gets 2 strings - str1 and str2, and returns a new string which contains both given strings.
+
+		For example, if the input is:
+
+		Str1 = “She”
+
+		Str2 = “Codes”
+
+		The result should be:
+
+		“SheCodes”
+	 * 
+	 * Copy the following lines 
+	 * public static int combineStrings(String str1, String str2) {
+
+           StringBuilder sb = new StringBuilder();
+
+           sb.append(str1);
+
+           sb.append(str2);
+
+           return sb.toString();
+      }
+       Step b:
+
+		you need to change something in the code, 
+		so that the function will reproduce the requested result.
+
+		Hint - If you press the ‘check’ button and try to run the code as 
+		it is now, you will get the following error:
+
+       error: incompatible types: String cannot be converted to int
+
+		Step c:
+
+	   when you have finished, 
+	   click on the 'check' button to ensure that your code is correct.
+
+
+	 */
+	//We changed the Signature from int(return value) to String
+	public static String combineStrings(String str1, String str2) {
+		System.out.println("Quiz - Appending 2 Strings together");
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(str1);
+
+        sb.append(str2);
+        
+        String sheCodes = sb.toString();
+        
+        System.out.println(sheCodes);
+        System.out.println("--------------End Quiz------------------------------");
+
+        return sheCodes;
+   }
+
 }
