fda8147faab5d2d21238911132a93cbb91779cc3
ChenShahaf <cshahaf@gmail.com>
2023-01-15 22:54:01 +0200

    updated lesson3

--- src/Lesson03.java
+++ src/Lesson03.java
@@ -1,6 +1,72 @@
 import java.util.Scanner;
 
 public class Lesson03{
+	
+	public  void Exercise1() {
+
+		/*
+		 * Exercise 1 - Change the variables in the first section, so that each if statement resolves as true.
+		 *
+			String a = new String("Wow");
+        	String b = "Wow";
+        	String c = a;
+        	String d = c;
+
+        	boolean b1 = a == b;
+        	boolean b2 = d.equals(b + "!");
+        	boolean b3 = !c.equals(a);
+
+        	if (b1 && b2 && b3) {
+            	System.out.println("Success!");
+        	}
+		 */
+		//Removed the Object and made a to variable
+		String a = "Wow";
+		//b is the same as is was in the begning and now equals a
+    	String b = a;
+    	//in order so c wont be equal to a we made c equals "Wow!"
+    	String c = b + "!";
+    	//In order for d to be equal to b+ "!" we need to keep d = c
+    	String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }	
+	}
+	public  void ageInFiveYears() {
+    	/*
+    	 * 1. Ask the user for their name. 
+    	 * 2. Then display their name to prove that you can recall it. 
+    	 * 3. Ask them for their age. 
+    	 * 4. Then display what their age would be five years from now. 
+    	 * 5. Then display what their age would be five years ago.
+    	 */
+		
+		//We need to use Scanner Object for this Excerise - 
+		//we imported the scanner library in the top of the page
+		
+		//Step 1 Creating a new Scanner object to get input from the user
+		Scanner input = new Scanner(System.in);
+		//Print a message to the screen to the user to recieve his inputs
+		System.out.println("Hi please type in your name followed by enter: ");
+		//we creating a variable name to receive the input using the scanner object
+		String name  = input.nextLine();
+		//Step 2 - Display the name we receievd as an input
+		System.out.println("Hi " + name);
+		//Step 3 - asking for the user age
+		System.out.println("Please type in your age followed by enter: ");
+		int age = input.nextInt();
+		//now we finished receive data we will close the scanner
+		input.close();
+		//Step 4 - displaying the user age 5 years from now
+		System.out.println("Your age in 5 years will be: " + (age+5));
+		//Step 5 - display the user age 5 years ago
+		System.out.println("Your age 5 years ago was: " + (age-5));		
+    }
 
 	public void HowOldAreYou(){
 		//creating a new scanner to receive inner input, I called it keyboard because the user types in using a keyboard.
